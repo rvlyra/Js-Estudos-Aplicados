d1997648b94e12a023e213a4aeff1eaa4cd7046a
Rubens Lyra <rubensvlyra@gmail.com>
2022-07-09 14:35:27 -0300

    Aula 08 - map, filter e reduce

--- 08-Arrays_Map_Filter_Reduce/script.js
+++ 08-Arrays_Map_Filter_Reduce/script.js
@@ -1,12 +1,82 @@
-// fetch("https://jsonplaceholder.typicode.com/todos/1")
-//   .then((response) => response.json())
-// .then((json) => console.log(json));
+const precos = [
+  "Crédito",
+  "R$ 200",
+  "R$ 400",
+  "Contas Pagar",
+  "R$ 300",
+  "R$ 400",
+  "Meus dados",
+];
 
-async function apiUser(url) {
-  const response = await fetch(url);
-  const jsonBody = await response.json();
-  //return jsonBody;
-  console.log(response);
-}
+// Reduce
 
-apiUser("https://jsonplaceholder.typicode.com/users");
+const precosFiltro = precos.filter((preco) => preco.includes("R$"));
+
+const precosNumeros = precosFiltro.map((preco) =>
+  Number(preco.replace("R$ ", ""))
+);
+
+const total = precosNumeros.reduce((acc, item) => acc + item);
+
+console.log(total);
+
+/**
+// Map
+
+const precosFiltro = precos.filter((preco) => preco.includes("R$"));
+
+const precosNumeros = precosFiltro.map((preco) => {
+  console.log(preco);
+  //return preco.replace("R$ ", "");
+
+  //Para transformar uma string em um número, basta adicionar o sinal de soma (+) à frente da variável. 
+  // Podemos optar por escrever Number e o resultado é mesmo
+  return Number(preco.replace("R$ ", ""));
+});
+
+console.log(precosNumeros);
+*/
+// Filter
+
+/**
+const precosFiltro = precos.filter((preco) => {
+  console.log(preco.includes("R$"));
+  preco === "R$" ? true : false;
+  return true;
+});
+
+// Quando há apenas 1 elemento, podemos usar a seguinte estrutura?
+const precosFiltro = precos.filter((preco) => preco.includes("R$"));
+
+console.log(precosFiltro);
+*/
+/**
+
+ const precos = [
+   "Crédito",
+  "R$ 200",
+  "R$ 400",
+  "Contas Pagar",
+  "R$ 300",
+  "R$ 400",
+  "Meus dados",
+];
+
+// Retorna uma array nova, que contem os items em
+// que o retorno da função for verdadeiro
+const precosFiltro = precos.filter((preco) => preco.includes("R$"));
+
+// Retorna uma nova array, modificada com o
+// retorno de cada item da função
+const precoNumeros = precosFiltro.map((preco) =>
+  Number(preco.replace("R$ ", ""))
+);
+
+// Retorna um valor um único, o valor retornado
+// fica disponível na próxima iteração através do
+// primeiro argumento, por isso o nome acumulador
+const total = precoNumeros.reduce((acc, item) => acc + item);
+
+console.log(total);
+
+*/
